df414c49c374c86df5f778a53ee4b1a0b3d85111
ahnstu <83620557+ahnstu1@users.noreply.github.com>
2025-02-05 14:25:55 +0900

    [Bronze IV] Title: 베라의 패션, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		System.out.println(input * (input - 1));
+		
+	}
+}
