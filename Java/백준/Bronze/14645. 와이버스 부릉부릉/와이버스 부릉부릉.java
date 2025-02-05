7b7d5ff7150df2937b21b9e26b6cabfe8df69cc5
ahnstu <83620557+ahnstu1@users.noreply.github.com>
2025-02-05 14:25:11 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 196 ms, Memory: 17520 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sc.nextLine();
+		sc.nextLine();
+		sc.nextLine();
+		sc.nextLine();
+		
+		System.out.println("비와이");
+		
+	}
+}
