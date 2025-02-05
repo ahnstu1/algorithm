d9dbd75d92d6c3477edabb49a3f45c2d471d8d2c
ahnstu <83620557+ahnstu1@users.noreply.github.com>
2025-02-05 11:39:20 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 436 ms, Memory: 20028 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main{
+    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+		
+		int inputNumber = sc.nextInt();
+		
+		String str = "";
+		
+		for(int i = 0; i < inputNumber; i++) {
+			
+			str += "*";
+			
+			System.out.printf("%" + inputNumber + "s\n", str);
+			
+		}
+		
+		for(int i = inputNumber - 1; i > 0; i--) {
+			
+			str = str.substring(1);
+			
+			System.out.printf("%" + inputNumber + "s\n", str);
+			
+		}
+        
+    }
+    
+}
