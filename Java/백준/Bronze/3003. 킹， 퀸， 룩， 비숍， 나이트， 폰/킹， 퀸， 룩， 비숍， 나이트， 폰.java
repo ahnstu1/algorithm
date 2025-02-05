bda2f81b6f0da679bc96c8ac531e9ab415ce3c0c
ahnstu <83620557+ahnstu1@users.noreply.github.com>
2025-02-05 14:08:07 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 296 ms, Memory: 19064 KB -BaekjoonHub

---
+++ Java/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main{
+    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println((1 - sc.nextInt()) + " " + (1 - sc.nextInt()) + " " + (2 - sc.nextInt()) + " " + (2 - sc.nextInt()) + " " + (2 - sc.nextInt()) + " " + (8 - sc.nextInt()));
+        
+    }
+    
+}
